dd4ea03dab9a89bcb11dca6625fc350331a45b34
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-12-06 22:04:56 -0600

    Replace evoteams with ratchets

--- ratchets/R/kaggle.R
+++ ratchets/R/kaggle.R
@@ -3,16 +3,16 @@
 #' @param sqlite_location Location of Kaggle sqlite database. Download this file
 #'   from \url{https://www.kaggle.com/kaggle/meta-kaggle/downloads/database.sqlite.zip},
 #'   unzip it, and put it wherever you'd like. By default, the file is expected
-#'   to be found in the "evoteams" package at "inst/extdata/kaggle.sqlite".
+#'   to be found in the "ratchets" package at "inst/extdata/kaggle.sqlite".
 #'
 #' @examples
 #' kaggle_db <- connect_kaggle("path/to/kaggle.sqlite")
-#' kaggle_db <- connect_kaggle()  # assumes db included in evoteams pkg
+#' kaggle_db <- connect_kaggle()  # assumes db included in ratchets pkg
 #'
 #' @export
 connect_kaggle <- function(sqlite_location) {
   if (missing(sqlite_location)) {
-    sqlite_location <- system.file("extdata/kaggle.sqlite", package = "evoteams")
+    sqlite_location <- system.file("extdata/kaggle.sqlite", package = "ratchets")
   }
   if (!file.exists(sqlite_location)) stop("kaggle.sqlite not found")
 
